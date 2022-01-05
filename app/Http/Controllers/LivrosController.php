d21b4282d3b8c485b8165bbf4bfa42b8e66575bc
LiviaSeferin <liva.santos@vtex.com.br>
2022-01-05 14:45:45 -0300

    first commit

---
+++ app/Http/Controllers/LivrosController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Livro;
+
+class LivrosController extends Controller
+{
+    public function create(){
+        return view( 'livros.create');
+    }
+    public function store(Request $request){
+        Livro::create([
+            'nome' => $request->nome,
+            'autor' => $request->autor,
+            'preco' => $request->preco,
+        ]);
+        return "Livro salvo com sucesso";
+    }
+    public function show(){
+        $livros = Livro::all();
+        return view('livros.todos',['livros' => $livros]);
+    }
+    public function destroy($id){
+        $livro=Livro::findOrFail($id);
+        $livro->delete();
+        return "Livro excluído com sucesso.";
+
+    }
+    public function edit($id){
+        $livro = Livro::findOrFail($id);
+        return view('livros.editar', ['livro' => $livro]);
+    }
+    public function update(Request $request, $id){
+        $livro = Livro::findOrFail($id);
+        $livro->update([
+            'nome' => $request->nome,
+            'autor' => $request->autor,
+            'preco' => $request->preco,
+        ]);
+        return "Livro atualizado com sucesso.";
+    }
+}
